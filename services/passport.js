00c4a917202b376e6bda221240ec0dd36a94cbd0
Katharine Francis <katie-francis@hotmail.com>
2018-11-21 15:05:26 -0500

    Existing User

--- services/passport.js
+++ services/passport.js
@@ -13,8 +13,14 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      const user = new User({ googleId: profile.id });
-      user.save();
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          const newUser = new User({ googleId: profile.id });
+          newUser.save().then(user => done(null, user));
+        }
+      });
     }
   )
 );
