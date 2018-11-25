6b6e9227801526b6fd0ab6c713eaaeb45fcf6ef9
Katharine Francis <katie-francis@hotmail.com>
2018-11-25 10:52:43 -0500

    Route Landing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,20 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveryNow = () => <h2>SurveryNow</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-  return <div>Hi There!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" component={Landing} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 export default App;
