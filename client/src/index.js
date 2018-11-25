ec3469f8bcb27e445377cddaffe17312ca83aa80
Katharine Francis <katie-francis@hotmail.com>
2018-11-25 10:39:22 -0500

    Combine reducers

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,9 @@ import ReactDom from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import App from "./components/App";
+import reducers from "./reducers";
 
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDom.render(
   <Provider store={store}>
