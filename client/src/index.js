0be78596a490e58f69091bf36319d67d9493724e
Katharine Francis <katie-francis@hotmail.com>
2019-01-10 15:03:29 -0800

    Fixed redirect issue

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,8 @@ import reduxThunk from "redux-thunk";
 
 import App from "./components/App";
 import reducers from "./reducers";
+
+//Devolopment only axios helpers!
 import axios from "axios";
 window.axios = axios;
 
