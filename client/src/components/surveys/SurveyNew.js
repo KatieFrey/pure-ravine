c41cea923f40e126b97e47b5ead081fc83116c65
Katharine Francis <katie-francis@hotmail.com>
2019-01-16 16:43:18 -0500

    Field Component inside SurveyForm

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,8 +1,10 @@
+//SurveyNew shows SurveyForm and SurveyFormReview
 import React, { Component } from "react";
+import SurveyForm from "./SurveyForm";
 
 class SurveyNew extends Component {
   render() {
-    return <div>SurveyNew!</div>;
+    return <SurveyForm />;
   }
 }
 
